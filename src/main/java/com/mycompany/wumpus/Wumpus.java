40302ca0ba9cd2e77dc8953c25a562c328fbf316
szilpatrik23 <119936032+szilpatrik23@users.noreply.github.com>
2023-12-07 18:31:21 +0100

    ...

--- src/main/java/com/mycompany/wumpus/Wumpus.java
+++ src/main/java/com/mycompany/wumpus/Wumpus.java
@@ -1,15 +1,9 @@
 
 package com.mycompany.wumpus;
 
-import Main.Menu;
-
-
-
 public class Wumpus {
 
     public static void main(String[] args) {
-        Menu menu = new Menu();
-        menu.fomenu();
-       
+        
     }
 }
