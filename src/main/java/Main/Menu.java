e912af9fc0cb2fbc17e494e28f35bc2fcad0e810
szilpatrik23 <119936032+szilpatrik23@users.noreply.github.com>
2023-12-07 19:28:36 +0100

    ...

--- src/main/java/Main/Menu.java
+++ src/main/java/Main/Menu.java
@@ -1,4 +1,74 @@
 package Main;
 
+import java.util.Scanner;
 public class Menu {
-         
+    private final Scanner scan = new Scanner(System.in);
+    
+    //Game game = new Game();
+    
+    public void FoMenu()
+    {
+     FoMenuKiiras();
+     int valasztas = scan.nextInt();
+        switch (valasztas) {
+            case 1:
+                JatekMenu();
+                break;
+            case 2:
+                System.out.println("Kilépés a játékból!");
+                System.exit(0);
+                break;
+            default:
+                System.out.println("Érvénytelen karakter!");
+                FoMenu();
+        }
+    }
+    
+    public void JatekMenu(){
+        JatekMenuKiiras();
+        int valasztas = scan.nextInt();
+        switch (valasztas) {
+            case 1:
+                
+                //Game.startgameplay();
+                break;
+            case 2:
+                System.out.println("Fejlesztés alatt!");
+                break;
+            case 3:
+               FoMenu();
+               break;
+            default:
+                System.out.println("Érvénytelen karakter!");
+                JatekMenu();
+        }
+    }
+    
+    public void FoMenuKiiras()
+    {
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        System.out.println("||                            ||");
+        System.out.println("||        |ÜDVÖZÖLLEK!|       ||");
+        System.out.println("||                            ||");
+        System.out.println("||         |FŐMENÜ|           ||");
+        System.out.println("||                            ||");
+        System.out.println("||        1. Játék            ||");
+        System.out.println("||        2. Kilépés          ||");
+        System.out.println("||                            ||");
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+    }
+    
+    public void JatekMenuKiiras()
+    {
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        System.out.println("||                            ||");
+        System.out.println("||         |FŐMENÜ|           ||");
+        System.out.println("||                            ||");
+        System.out.println("|| 1. Pálya választás fájlból ||");
+        System.out.println("|| 2. Pálya választás adbból  ||");
+        System.out.println("||                            ||");
+        System.out.println("||         3. Főmenű          ||");
+        System.out.println("||                            ||");
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+    }
+}      
--- src/main/java/com/mycompany/wumpus/Wumpus.java
+++ src/main/java/com/mycompany/wumpus/Wumpus.java
@@ -1,9 +1,13 @@
 
 package com.mycompany.wumpus;
 
+import Main.Menu;
+
+
 public class Wumpus {
 
     public static void main(String[] args) {
-        
+        Menu menu = new Menu();
+        menu.FoMenu();
     }
 }
