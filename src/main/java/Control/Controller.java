f20457a12e56dfa784a50017e8a96f6959f2ce35
szilpatrik23 <119936032+szilpatrik23@users.noreply.github.com>
2023-12-07 17:06:41 +0100

    little things

---
+++ src/main/java/Control/Controller.java
@@ -0,0 +1,7 @@
+package Control;
+
+public class Controller {
+    
+    private int step;
+    
+   }
---
+++ src/main/java/Main/User.java
@@ -0,0 +1,55 @@
+package Main;
+public class User {
+    private final Scanner scan;
+    
+    private int pont;
+    private String felhasznev;
+    
+    public User(Scanner scan){
+        this.scan = scan;
+        this.pont = 0;
+        this.felhasznev = "";
+    }
+    
+    public String FelnevBeker(){
+        String input;
+        do{
+            input = FelhasznalotolBeker();
+            if(!joFelhasznalonev(input))
+            {
+                System.out.println("Hibás felhasználónév!");
+            }
+          }
+        while(joFelhasznalonev(input));
+        
+        
+        felhasznev = input;
+        return felhasznev;
+    }
+    
+    
+    private String FelhasznalotolBeker(){
+        System.out.println("Kérlek add meg a felhasználóneved ami 20 karakter hosszú lehet!");
+        return scan.nextLine();
+    }
+    
+    private boolean joFelhasznalonev(String username){
+        return username.length() >= 2 && username.length() <= 20;
+    }
+    
+    public String getFelhasznev()
+    {
+        return felhasznev;
+    }
+    public int getPont(){
+        return pont;
+    }
+    public void pontNovelo(){
+        pont++;
+    }
+    
+    public String getFelhasznaloPonttal()
+    {
+        return ("Kedves "+ getFelhasznev() +" ennyi pontod van jelenleg: "+getPont());
+    }
+}
